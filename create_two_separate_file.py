3837945990f8781db217599a527aa2a151a9282e
james malitan <jamesmalitan21@gmail.com>
2026-04-27 11:15:40 +0800

    building_the_blueprint_of_the_program

---
+++ create_two_separate_file.py
@@ -0,0 +1,7 @@
+class FileSeparator:
+    def __init__(self, input_file, even_file, odd_file):
+        self.input_file = input_file
+        self.even_file = even_file
+        self.odd_file = odd_file
+
+   
